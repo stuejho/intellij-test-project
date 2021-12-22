c7dba545e0e92b36ded9fdcd3428e77cea77ceeb
Ethan Ho <ethan.j.ho-1@ou.edu>
2021-12-22 11:37:40 -0600

    Create a, b, and c methods

--- src/Main.java
+++ src/Main.java
@@ -3,4 +3,16 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    public static void a() {
+
+    }
+
+    public static void b() {
+
+    }
+
+    public static void c() {
+
+    }
 }
