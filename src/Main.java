daa27bcd99cce40fab50c5e4af242dadcb76435d
Ethan Ho <ethan.j.ho-1@ou.edu>
2021-12-22 12:06:13 -0600

    Add print statements to methods Each method prints its name

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
 public class Main {
 
     public static void a() {
-
+        System.out.println("Hi from a");
     }
 
     public static void b() {
-
+        System.out.println("Hi from b");
     }
 
     public static void c() {
-
+        System.out.println("Hi from c");
     }
 
     public static void main(String[] args) {
